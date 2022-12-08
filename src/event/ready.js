1245ba78bb3f384e14058d71b073bf71fafed488
Cyclops789 <70551889+Cyclops789@users.noreply.github.com>
2022-12-08 02:55:59 +0100

    Update ready.js

--- src/event/ready.js
+++ src/event/ready.js
@@ -3,5 +3,5 @@ const { prefix } = require("../../config");
 module.exports = async (client) => {
     client.logger.info(`[!] ${client.user.username} is now started...`)
     client.logger.info(`[!] The bot have ${client.commandes.size} commands and ${client.slash.size} (/) commands`)
-    client.user.setActivity(`${prefix}help | github.com/meliooff`, { type: 'PLAYING' })
+    client.user.setActivity(`github.com/Cyclops789/Gameme-discord-bot`, { type: 'PLAYING' })
 };
