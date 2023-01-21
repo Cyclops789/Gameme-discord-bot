07a403f7b47cdc664dcdcc7ffb6ae4b70524eb5c
Cyclops789 <70551889+Cyclops789@users.noreply.github.com>
2023-01-21 21:56:44 +0100

    🔐

--- config.example.js
+++ config.example.js
@@ -13,10 +13,10 @@ module.exports = {
     embedColor: "black",
 
     // set this to your gameme page, it shouldn't end with /
-    gamemelink: "http://stats.wafgamers.fun",
+    gamemelink: "https://stats.wafgamers.fun",
 
     // it convert xml to json, credit: "https://github.com/factmaven/xml-to-json" , you can use "https://api.factmaven.com/xml-to-json/?xml=" as well
-    apilink: "http://api.wafgamers.fun/xml/?xml=",
+    apilink: "https://api.wafgamers.fun/xml/?xml=",
 
     // api paths, dont change anything here!!
     playerinfo: "/api/playerinfo/",
